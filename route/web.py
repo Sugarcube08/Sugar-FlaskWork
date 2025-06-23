d0c3059913545c6349f3e9948b416ba7f30fe397
Sugar Cube <hraikwar861@gmail.com>
2025-06-24 00:56:01 +0530

    regular update

--- route/web.py
+++ route/web.py
@@ -1,5 +1,7 @@
 from controller import HomeController
 
+home_controller = HomeController()
+
 def setupRoute(app):
-    app.add_url_rule('/',             endpoint='home',         view_func=HomeController.index,        methods=['GET'])
+    app.add_url_rule('/',             endpoint='home',         view_func=home_controller.index,        methods=['GET'])
     
