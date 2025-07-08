4f95f68ac394660b21f2416ec50eb08640102b25
Sugar Cube <hraikwar861@gmail.com>
2025-07-09 02:04:54 +0530

    encrypted app.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-from .models import model
+__all__ = []
+from .user import User
+__all__.append('User')
+from .admin import Admin
+__all__.append('Admin')
+from .userprofile import UserProfile
+__all__.append('Userprofile')
+from .attendancetoken import Attendancetoken
+__all__.append('Attendancetoken')
+from .attendancerecord import Attendancerecord
+__all__.append('Attendancerecord')
---
+++ models/admin.py
@@ -0,0 +1,13 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)  # Must match the actual column name
+
+    def set_password(self, plain_password):
+        self.password = generate_password_hash(plain_password)
+
+    def check_password(self, plain_password):
+        return check_password_hash(self.password, plain_password)
